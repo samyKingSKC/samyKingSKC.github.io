f5d982a8cc95efe5efca284a0becb824c27dc3db
kingsley samuel <204401205+samyKingSKC@users.noreply.github.com>
2025-10-11 01:09:37 +0100

    u

---
+++ src/components/EmailPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <a href="mailto:fedrickjohnson642@gmail.com">
+      <div
+        class=" fixed bottom-[10vh] right-[10vw] z-[1000]  w-[1.5cm] h-[1.5cm] rounded-full flex items-center justify-center bg-primary"
+      >
+       
+        <i class="fa fa-envelope text-blue-800 !text-2xl animate__animated animate__infinite animate-ping"></i>
+      </div>
+    </a>
+  </div>
+</template>
+<script setup>
+
+
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Home">
+    <NavPage />
+    <EmailPage />
+    <ContentPage />
+  </div>
+</template>
+
+<script setup>
+import NavPage from "../components/NavPage.vue";
+import ContentPage from "../components/ContentPage.vue";
+import EmailPage from "../components/EmailPage.vue";
+
+</script>
+
+<style></style>
